a971ec45f2c33e96dda6440155bd11c8acc8b4b5
Roberto <111545434+Jamiro-MR@users.noreply.github.com>
2023-02-21 09:50:26 -0600

    Showing imgs
    Mostrar los objetos en venta con JSON y JS

--- ejercio1/resources/js/app.js
+++ ejercio1/resources/js/app.js
@@ -1,32 +1,38 @@
 const app = {
+ urlDatos : "/resources/data/data.json",
+ filtro : "todos",
 
-	urlDatos : "/resources/data/data.json"
-	filtro : "todos",
+ cargarFichas : function() {
+  const fichas = document.querySelector("#fichas");
+  let html = "";
+  fetch(this.urlDatos)
+   .then(response => response.json())
+   .then(autos => {
+    for (auto of autos){
+     if(auto.tipo === this.filtro || this.filtro === "todos"){
+      html += `
+       <div class="ficha">
+         <img src="./resources/imgs/${auto.foto}" alt="${auto.marca}">        
+         <div class="datos">
+         <h3>${auto.marca}</h3>
+         <span>${auto.modelo}</span>
+         <span>${auto.año}</span>
+         <br>
+         <small>
+          ${auto.motor.potencia}, 
+          ${auto.motor.kilometraje}, 
+          ${auto.motor.desplazamiento}
+         </small>
+        </div>
+       </div>
+      `;
+     }
+    }
+    fichas.innerHTML = html;
+   }).catch(err => console.error(err));
+  }
+}
 
-	cargarFichas : function(){
-		const fichas = document.querySelector("#fichas");
-		let html = "";
-		fetch(this.urlDatos)
-			.then( response => response.json())
-			.then( autos => {
-				for(auto of autos){
-					if(auto.tipo === this.filtro || this.filtro === "todos"){
-						html += `
-							<div class="ficha">
-								<img src="./resources/imgs/${auto.foto}" alt="${auto.marca}">
-								<div class="datos">
-									<h3>${auto.marca}</h3>
-									<span>${auto.modelo}</span>
-									<span>${auto.añp}</span>
-									<br>
-									<small>${auto.potencia}, ${auto.kilometraje}, ${auto.desplazamiento}</small>
-								</div>
-							</div>
-						`;
-					}
-				}
-				fichas.innerHTML = html;
-			}).catch(err => console.error( err ));
-	}
-
+window.onload = function(){
+ app.cargarFichas();
 }
