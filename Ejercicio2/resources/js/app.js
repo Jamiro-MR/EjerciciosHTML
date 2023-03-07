704b8914e63309a76d86777e468d1208a3437636
Roberto <111545434+Jamiro-MR@users.noreply.github.com>
2023-03-07 08:57:55 -0600

    a

--- Ejercicio2/resources/js/app.js
+++ Ejercicio2/resources/js/app.js
@@ -1,5 +1,7 @@
 const app = {
 	urlPosts : "https://jsonplaceholder.typicode.com/posts",
+	urlComments: "https://jsonplaceholder.typicode.com/comments",
+	urlUsers : "https://jsonplaceholder.typicode.com/users",
 
 	loadPosts : function() {
 		// const cont = document.querySelector("#content");
@@ -13,7 +15,7 @@ const app = {
 				for ( let post of posts ){
 					//let autor
 					html+=`			
-					<div class="card text-center">
+					<div class="card text mb-3">
 						<div class="card-header">
 							${ post.title }
 						</div>
@@ -21,15 +23,72 @@ const app = {
 							<p class="card-text"> ${ post.body }</p>
 						</div>
 						<div class="card-footer text-muted">
-						<a href="#" class="btn btn-primary">Like</a>
+							<button class="btn btn-primary btn-link" type="button"
+								id="btn-ver-comms${ post.id}"
+								onclick="app.loadComments(${ post.id})">
+								View comments
+							</button>
+							<button class="btn btn-link d-none" type="button"
+								id="btn-cls-com${ post.id }"
+								onclick="app.closeComments( ${ post.id })">
+								Close Comments
+							</button>
+							<div class="card d-none" id="card-comments${ post.id}">
+								<ul class="list-group list-group-flush" id="comments${ post.id}"
+								</ul>
+							</div>
 						</div>
 					</div>
 					`;
 					cont.html(html);
 				}
 			} ).catch( err => console.error(err) )
+	},
+	loadComments : function(postId){
+		let html = "";
+		const listComms = $("#comments" + postId);
+		fetch(this.urlComments + "?postId=" + postId)
+			.then( resp => resp.json())
+			.then( comments => {
+				for( let c of comments ){
+					html += `
+						<li class="list-group-item">
+							De: <b>${ c.email }</b> <br>${ c.body }
+						</li>`
+				}
+				listComms.html(html);
+				$("#card-comments" + postId).toggleClass("d-none", false);
+				$("#btn-ver-comms" + postId).toggleClass("d-none", true);
+				$("#btn-cls-com" + postId).toggleClass("d-none", false);
+			}).catch( err => console.error(err));
+	},
+	closeComments : function( postId ){
+		$("#comments" + postId).html("");
+		$("#card-comments" + postId).toggleClass("d-none", true);
+		$("#btn-ver-comms" + postId).toggleClass("d-none", false);
+		$("#btn-cls-com" + postId).toggleClass("d-none", true);
+	},
+	loadUsers : function(){
+		const users = $("#authors");
+		let html="";
+		users.addClass("mt-5");
+		users.html(html);
+		fetch(this.urlUsers)
+			.then( resp => resp.json() )
+			.then( usuarios => {
+				for( let u of usuarios ){
+					html += `
+						<button type="button" class="list-group-item list-group-item-action" aria-current="true"
+							id="usp${u.id}"
+							onclick="app.userPosts( ${ u.id } )">
+						    ${ u.name } <br><small>${ u.email }</small>
+						</button>`;
+				}
+				users.html(html);
+			} ).catch( err => console.error( err ) );
 	}
 }
 window.onload = function(){
 	app.loadPosts();
+	app.loadUsers();
 }
--- Ejercicio2/resources/views/layouts/main.php
+++ Ejercicio2/resources/views/layouts/main.php
@@ -43,13 +43,13 @@
 		        </li>
 		        <li class="nav-item dropdown">
 		          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-		            Dropdown
+		            Secciones
 		          </a>
 		          <ul class="dropdown-menu">
-		            <li><a class="dropdown-item" href="#">Action</a></li>
-		            <li><a class="dropdown-item" href="#">Another action</a></li>
+		            <li><a class="dropdown-item" href="#">Autos</a></li>
+		            <li><a class="dropdown-item" href="#">Tsurus</a></li>
 		            <li><hr class="dropdown-divider"></li>
-		            <li><a class="dropdown-item" href="#">Something else here</a></li>
+		            <li><a class="dropdown-item" href="#">mamalones</a></li>
 		          </ul>
 		        </li>
 		      </ul>
