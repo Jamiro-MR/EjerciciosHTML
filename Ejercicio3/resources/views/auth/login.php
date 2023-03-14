00a1ff76d1340347ea74139f0921346fa97d70e0
Roberto <111545434+Jamiro-MR@users.noreply.github.com>
2023-03-14 08:52:48 -0600

    aaaaaaaaaaaaaaa ejercicio 3

---
+++ Ejercicio3/resources/views/auth/login.php
@@ -0,0 +1,37 @@
+<?php
+	namespace views;
+	//require "autoloader.php";
+	include "../layouts/main.php";
+	//use Controllers\auth\LoginController as LoginController;
+
+	head();
+?>
+
+<div class="container">
+	<div class="card mt-5 w-50 mx-auto">
+		<div class="card-body">
+			<form action="" id="login-form">
+				<div class="form-group">
+					<label for="username">Usuario</label>
+					<input type="text" 
+					       id="username"
+					       class="form-control"
+					       name="username"
+					       placeholder="Nombre de usuario"
+					       required>
+				</div>
+				<div class="form-group">
+					<label for="passwd">Contraseña</label>
+					<input type="password"
+					       class="form-control"
+					       id="passwd"
+					       name="passwd"
+					       required>
+				</div>
+				<small class="form-text text-danger d-none mb-2">
+					Los datos ingresados en el inicio de sesión son incorrectos.
+				</small>
+			</form>
+		</div>
+	</div>
+</div>
